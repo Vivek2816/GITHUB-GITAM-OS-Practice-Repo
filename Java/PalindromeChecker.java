33f84f42c0906efcfc14fb85b9adc8f29dd2e6b6
Vivek2816 <vdoddi2@gitam.in>
2025-09-09 09:58:55 +0530

    Implemented palindrome check using string reversal

--- Java/PalindromeChecker.java
+++ Java/PalindromeChecker.java
@@ -2,31 +2,18 @@
 package Java;
 import java.util.Scanner;
 
-class Palindrome {
-    boolean isPalindrome(String str) {
-        int left = 0;
-        int right = str.length() - 1;
-
-        while (left < right) {
-            if (str.charAt(left) != str.charAt(right)) {
-                return false;
-            }
-            left++;
-            right--;
-        }
-        return true;
-    }
-}
-
 public class PalindromeChecker {
+    public static boolean isPalindrome(String str) {
+        String reversed = new StringBuilder(str).reverse().toString();
+        return str.equals(reversed);
+    }
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        Palindrome t = new Palindrome();
         System.out.print("Enter a string: ");
         String input = sc.nextLine();
-        boolean result = t.isPalindrome(input);
 
-        if (result) {
+        if (isPalindrome(input)) {
             System.out.println(input + " is a palindrome.");
         } else {
             System.out.println(input + " is not a palindrome.");
@@ -34,5 +21,3 @@ public class PalindromeChecker {
         sc.close();
     }
 }
-
-//try doing it in some other way
