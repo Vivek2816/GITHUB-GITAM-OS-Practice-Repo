559c9cbc507663923e2b4c32f4f860048b001bf6
Vivek2816 <vdoddi2@gitam.in>
2025-09-09 09:39:30 +0530

    Add iterative factorial function alongside recursive version

--- Python/factorial.py
+++ Python/factorial.py
@@ -1,14 +1,16 @@
-# easy-level
-
-def factorial(n):
+def factorial_recursive(n):
     if n == 0 or n == 1:
         return 1
     else:
-        return n * factorial(n - 1)
+        return n * factorial_recursive(n - 1)
+
+def factorial_iterative(n):
+    result = 1
+    for i in range(2, n + 1):
+        result *= i
+    return result
 
 num = int(input("Enter a number: "))
 
-print(f"Factorial of {num} is {factorial(num)}")
-
-
-# trying solving it iteratively 
+print(f"Factorial of {num} (recursive) is {factorial_recursive(num)}")
+print(f"Factorial of {num} (iterative) is {factorial_iterative(num)}")
